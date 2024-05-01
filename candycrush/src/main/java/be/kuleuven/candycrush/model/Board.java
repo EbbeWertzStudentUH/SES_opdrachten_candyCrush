e70fdf25f16a70e692e9266e54f0469d06be66fd
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2024-05-01 14:25:30 +0200

    copyto gemaakt

--- candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
+++ candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
@@ -18,16 +18,18 @@ public class Board<T>{
 
     private BoardSize boardSize;
 
-    public Board() {
+    public Board(BoardSize boardSize) {
         board = new ArrayList<>();
     }
 
     //TODO cel op een gegeven positie van het bord op te vragen
     public T getCellAt(Position position){
+        validatePosition(position);
         return board.get(position.toIndex());
     }
     //TODO cel op een gegeven positie te vervangen door een meegegeven object
     public void replaceCellAt(Position position, T newCell){
+        validatePosition(position);
         board.set(position.toIndex(), newCell);
     }
     //TODO hele bord te vullen via func
@@ -37,7 +39,17 @@ public class Board<T>{
     //TODO alle cellen van het huidige bord kopieert naar het meegegeven bord
     //niet dezelfde afmetingen heeft, gooi je een exception
     public void copyTo(Board<T> otherBoard){
+        if(!otherBoard.boardSize.equals(boardSize)){
+            throw new IllegalArgumentException("Boardsize of other board: "+otherBoard.boardSize+" does not match the boardsize of this board:"+boardSize);
+        }
+        otherBoard.board.clear();
+        otherBoard.board.addAll(board);
+    }
 
+    private void validatePosition(Position position){
+        if(! position.boardSize().equals(boardSize)){
+            throw new IllegalArgumentException("Position: "+position+" does not match the boardsize of the board");
+        }
     }
 }
 
