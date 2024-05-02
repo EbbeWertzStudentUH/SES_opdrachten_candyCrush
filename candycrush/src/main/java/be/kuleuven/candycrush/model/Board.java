0e5eeff6423810bb73a8cad3d7e789fdf500b305
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2024-05-02 12:44:12 +0200

    Board een hashmap gemaakt

--- candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
+++ candycrush/src/main/java/be/kuleuven/candycrush/model/Board.java
@@ -3,45 +3,48 @@ package be.kuleuven.candycrush.model;
 import be.kuleuven.candycrush.model.records.BoardSize;
 import be.kuleuven.candycrush.model.records.Position;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Board<T>{
 
-    private final ArrayList<T> board;
+    private final Map<Position, T> board;
 
     private final BoardSize boardSize;
 
     public Board(BoardSize boardSize) {
         this.boardSize = boardSize;
-        board = new ArrayList<>();
+        board = new HashMap<>();
     }
     //om te clonen
     public Board(Board<T> board){
         this.boardSize = board.boardSize;
-        this.board = new ArrayList<>(board.board);
+        this.board = new HashMap<>(board.board);
     }
 
     public T getCellAt(Position position){
         validatePosition(position);
-        return board.get(position.toIndex());
+        return board.get(position);
     }
     public void replaceCellAt(Position position, T newCell){
         validatePosition(position);
-        board.set(position.toIndex(), newCell);
+        board.put(position, newCell);
     }
     public void fill(Function<Position, ? extends T> cellCreator){
         board.clear();
         for(Position position : boardSize.positions()){
-            board.add(cellCreator.apply(position));
+            board.put(position, cellCreator.apply(position));
         }
     }
     public void copyTo(Board<? super T> otherBoard){
         if(!otherBoard.boardSize.equals(boardSize)){
             throw new IllegalArgumentException("Boardsize of other board: "+otherBoard.boardSize+" does not match the boardsize of this board:"+boardSize);
         }
-        otherBoard.board.clear();
-        otherBoard.board.addAll(board);
+        for(Position position : boardSize.positions()){
+            T item = board.get(position);
+            otherBoard.board.put(position, item);
+        }
     }
 
     private void validatePosition(Position position){
